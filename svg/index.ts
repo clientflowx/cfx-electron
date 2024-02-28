026cdf35ff413bc549faa22349379f4f63d67f42
Anubhav Pal <anubhavpal15june@gmail.com>
2024-02-28 12:29:10 +0530

    Refactor Sidebar code

---
+++ svg/index.ts
@@ -0,0 +1,8 @@
+import Dashboard from "./Dashboard";
+import Components from "./Components";
+import Calendar from "./Calendar";
+import Files from "./Files";
+import Inbox from "./Inbox";
+
+
+export { Dashboard, Components, Calendar, Files, Inbox } 
