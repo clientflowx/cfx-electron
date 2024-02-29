c3a5bb6a5a386f2c8153f3e998cff8b6515194d5
Anubhav Pal <anubhavpal15june@gmail.com>
2024-02-29 12:27:17 +0530

    Update Admin-panel UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { IoSearch } from "react-icons/io5";
-import { Avatar, Grid, List, Mail, Notification } from '@/svg/index.ts'
+import { Avatar, Grid, Hamburger, List, Mail, Notification } from '@/svg/index.ts'
 
 
 const Navbar: React.FC = () => {
@@ -7,11 +7,12 @@ const Navbar: React.FC = () => {
         <Notification />,
         <List />,
         <Mail />,
-        <Avatar />, 
+        <Avatar />,
         <Grid />,
     ]
+        
     return (
-        <div className="w-full fixed flex  bg-white z-20 items-center justify-between py-3 pl-20 px-10 shadow-sm">
+        <div className="w-full fixed flex  bg-white z-20 items-center justify-between gap-3 sm:gap-0 py-3 pl-20 px-10 shadow-sm">
             <div className="flex flex-row items-center justify-start gap-3">
                 <div className="flex items-center justify-between bg-[#f0f4f7] rounded-md">
                     <div className="opacity-30 h-full p-3">
@@ -25,8 +26,9 @@ const Navbar: React.FC = () => {
                 </div>
             </div>
             <div className="flex items-center justify-between gap-5 ">
+                <div className="w-6 sm:hidden opacity-60 hover:opacity-100"><Hamburger /></div>
                 {navIcons.map(navItem => (
-                    <div className="relative">
+                    <div className="relative hidden sm:flex">
 
                         <div className="absolute -right-1 -top-1 bg-green-600 opacity-95 rounded-full w-2 h-2"></div>
                         <div className="relative font w-6 opacity-40 hover:opacity-90 transition-all cursor-pointer">
