fcdf2c7403652a7bbe4b55088cfd139a1d90344c
Anubhav Pal <anubhav.pal@clientflowx.com>
2024-03-20 19:51:57 +0530

    feat/select-all: Add DateRange Pick functionality

--- app/dashboard/(sub-accounts)/manange-sub-acc.tsx
+++ app/dashboard/(sub-accounts)/manange-sub-acc.tsx
@@ -7,6 +7,13 @@ import { IoSearch } from 'react-icons/io5'
 import CreateSubAccIcon from '@/svg/CreateSubAccIcon'
 import SubAccFeedback from '@/svg/SubAccFeedback'
 import Loader from '@/components/Loader'
+import { DateRangePicker, RangeKeyDict } from "react-date-range";
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { Calendar } from '@/svg'
+import RightArrow from '@/svg/RightArrow'
+
 
 const initialSubAccountData: subAccountDataType[] = [{
   id: "",
@@ -53,44 +60,56 @@ const initialSubAccountData: subAccountDataType[] = [{
 
 const ManageSubAccounts = () => {
   const [subAccountsData, setSubAccountsData] = useState<subAccountDataType[]>([]);
-  const [nameFilter, setNameFilter] = useState<string>("");
-  const [loading, setLoading] = useState(false); // New state to track loading
+  const [nameFilter, setNameFilter] = useState<string>('');
+  const [loading, setLoading] = useState(false);
+  const [showDateSelector, setShowDateSelector] = useState<boolean>(false);
+  const [dateRange, setDateRange] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection'
+  });
+
+  console.log("date: ", dateRange.startDate.getDay());
+
 
   const filteredSubAccountData = subAccountsData.filter((data: subAccountDataType) => {
-    const nameMatch: boolean = nameFilter === "" || data?.name?.toLowerCase().includes(nameFilter.toLowerCase());
+    const nameMatch: boolean = nameFilter === '' || data?.name?.toLowerCase().includes(nameFilter.toLowerCase());
     return nameMatch;
   });
 
   useEffect(() => {
-    try {
-      const fetchSubAccountDetails = async () => {
-        setLoading(true);
-        const token = document.cookie
-          .split(";")
-          .find((cookie) => cookie.trim().startsWith("token="));
-        const tokenValue = token ? token.split("=")[1] : "";
-        const response = await axios.get("https://cfx-mono-production-5ec7.up.railway.app/api/internal/get-agency-subaccounts?isDetail=true",
-          {
-            headers: {
-              Authorization: `Bearer ${tokenValue}`,
-            },
-          })
-
-        console.log("resposnse from api: ", response?.data?.data);
+    const fetchSubAccountDetails = async () => {
+      setLoading(true);
+      try {
+        const token = document.cookie.split(';').find((cookie) => cookie.trim().startsWith('token='));
+        const tokenValue = token ? token.split('=')[1] : '';
+        const response = await axios.get('https://cfx-mono-production-5ec7.up.railway.app/api/internal/get-agency-subaccounts?isDetail=true', {
+          headers: {
+            Authorization: `Bearer ${tokenValue}`
+          }
+        });
+        setSubAccountsData(response?.data?.data || []);
+      } catch (error) {
+        console.error('Error fetching sub account details:', error);
+      } finally {
         setLoading(false);
-        setSubAccountsData(response?.data?.data || [])
       }
-      fetchSubAccountDetails();
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-  }, [])
+    };
+    fetchSubAccountDetails();
+  }, []);
 
-  const handleNameFilter: React.ChangeEventHandler<HTMLInputElement> | undefined = (e) => {
-    const { name, value } = e.target;
+  const handleNameFilter: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { value } = e.target;
     setNameFilter(value);
-  }
+  };
+
+  const handleDateSelector = () => {
+    setShowDateSelector(!showDateSelector);
+  };
+
+  const handleDateChange = (ranges: any) => {
+    setDateRange(ranges.selection);
+  };
 
   return (
     <div className='flex flex-col w-full justify-between gap-4'>
@@ -111,7 +130,33 @@ const ManageSubAccounts = () => {
 
       {/* sub accounts filter */}
       <div className='w-full flex items-center justify-between'>
-        <div></div>
+        <div className="relative">
+          <div className="cursor-pointer flex items-center justify-start gap-2 shadow-sm border rounded-md bg-white p-2" onClick={handleDateSelector}>
+            <div className='text-sm font-semibold'>
+              {JSON.stringify(dateRange.startDate.getFullYear())}-{JSON.stringify(dateRange.startDate.getMonth())}-
+              {JSON.stringify(dateRange.startDate.getDate())}
+            </div>
+            <div className='w-3 h-3 opacity-50'>
+              <RightArrow/>
+            </div>
+            <div className='text-sm font-semibold'>
+              {JSON.stringify(dateRange.endDate.getFullYear())}-{JSON.stringify(dateRange.endDate.getMonth())}-
+              {JSON.stringify(dateRange.endDate.getDate())}
+            </div>
+            <div className=' w-5 h-5 opacity-50 mx-2 hover:opacity-75 transition-all'>
+              <Calendar />
+            </div>
+          </div>
+          {showDateSelector && (
+            <DateRangePicker
+              ranges={[dateRange]}
+              onChange={handleDateChange}
+              className='absolute shadow-sm border'
+              months={1}
+              direction='horizontal'
+            />
+          )}
+        </div>
         <div className='flex items-center justify-between'>
           <div className="flex items-center justify-between p-2 bg-white border border-1 outline-none rounded-md gap-1">
             <div className="opacity-30 h-full">
